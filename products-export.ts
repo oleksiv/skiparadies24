fd5d919fd1097a01ab3c14da2340078a183e2f5a
sviat <hi.oleksiv@gmail.com>
2023-11-02 18:18:24 +0100

    Scraped more products

--- products-export.ts
+++ products-export.ts
@@ -1,72 +1,96 @@
 import puppeteer from 'puppeteer';
 import * as cheerio from 'cheerio';
-import { CheerioAPI } from 'cheerio';
+import {CheerioAPI} from 'cheerio';
 import * as fs from "fs";
 
-const CATEGORY: string = 'zubehoer-wachs-bindungen';
+const CATEGORIES = [
+    'alpin-ski',
+    'eishockeyschlaeger',
+    'langlaufschuhe',
+    'langlaufski-und-schneeschuhe',
+    'langlaufstoecke-skistoecke',
+    'protektoren',
+    'schlitten-rutscher',
+    'schlittschuhe',
+    'skibrillen',
+    'skihelme',
+    'skischuhe',
+    'snowboardbindungen',
+    'snowboardboots',
+    'snowboards',
+    'zubehoer-wachs-bindungen',
+];
 (async () => {
-    const browserInstance = await puppeteer.launch({ headless: false });
+    const browserInstance = await puppeteer.launch({headless: false});
     const browserPage = await browserInstance.newPage();
-    await browserPage.setViewport({ width: 1200, height: 1024 });
+    await browserPage.setViewport({width: 1200, height: 1024});
 
-    await browserPage.goto(`https://www.sport-hopfmann.de/wintersport-sport/${CATEGORY}/?filter=f5:o100.00-99999`);
-    await browserPage.waitForSelector('#productList');
-
-    const pageHtml = await browserPage.content();
-    const loadedHtml = cheerio.load(pageHtml);
-
-    const skiProducts: { name: string, url: string }[] = [];
-    loadedHtml('.list-container .productBox').each((index, element) => {
-        const productName = loadedHtml(element).find('.productsTitle .hidden-xs.hidden-sm').text().trim();
-        const productUrl = loadedHtml(element).find('.productBox a').attr('href');
-        if (productName && productUrl) {
-            skiProducts.push({
-                name: productName,
-                url: productUrl,
-            });
+    for (let category of CATEGORIES) {
+        const fileExists = fs.existsSync(`data/${category}.json`)
+        if (fileExists) {
+            console.log(`data/${category}.json - already exist`)
+            continue;
         }
-    });
 
+        await browserPage.goto(`https://www.sport-hopfmann.de/wintersport-sport/${category}/?cl=alist&searchparam=&cnid=292&ldtype=grid&_artperpage=60&listorderby=oxvarminprice&listorder=desc&pgNr=0`);
+        await browserPage.waitForSelector('#productList');
 
-    const detailedProductData: { productColors: any[] }[] = [];
-    for (const product of skiProducts) {
-        await browserPage.goto(product.url);
-        await browserPage.waitForSelector('#productinfo');
+        const pageHtml = await browserPage.content();
+        const loadedHtml = cheerio.load(pageHtml);
 
-        const detailedPageHtml = await browserPage.content();
-        const detailedHtml = cheerio.load(detailedPageHtml);
-
-
-
-        const colorProductDetails = [];
-
-        const productDetails = extractProductDetails(detailedHtml, product.url);
-        colorProductDetails.push(productDetails);
-        console.log(`${productDetails.productName} - Done`);
-
-        const colorLinks: string[] = [];
-        detailedHtml('.colorContainer li:not(.active) a').each((index, element) => {
-            const colorLink = detailedHtml(element).attr('href') as string;
-            colorLinks.push(colorLink);
+        const skiProducts: { name: string, url: string }[] = [];
+        loadedHtml('.list-container .productBox').each((index, element) => {
+            const productName = loadedHtml(element).find('.productsTitle .hidden-xs.hidden-sm').text().trim();
+            const productUrl = loadedHtml(element).find('.productBox a').attr('href');
+            if (productName && productUrl) {
+                skiProducts.push({
+                    name: productName,
+                    url: productUrl,
+                });
+            }
         });
 
-        for (let colorLink of colorLinks) {
-            await browserPage.goto(colorLink);
-            await browserPage.waitForSelector('#productTitle');
-            const newDetailedPageHtml = await browserPage.content();
-            const newDetailedHtml = cheerio.load(newDetailedPageHtml);
-            const productDetails = extractProductDetails(newDetailedHtml, colorLink);
+
+        const detailedProductData: { productColors: any[] }[] = [];
+        for (const product of skiProducts) {
+            await browserPage.goto(product.url);
+            await browserPage.waitForSelector('#productinfo');
+
+            const detailedPageHtml = await browserPage.content();
+            const detailedHtml = cheerio.load(detailedPageHtml);
+
+
+            const colorProductDetails = [];
+
+            const productDetails = extractProductDetails(detailedHtml, product.url);
             colorProductDetails.push(productDetails);
             console.log(`${productDetails.productName} - Done`);
+
+            const colorLinks: string[] = [];
+            detailedHtml('.colorContainer li:not(.active) a').each((index, element) => {
+                const colorLink = detailedHtml(element).attr('href') as string;
+                colorLinks.push(colorLink);
+            });
+
+            for (let colorLink of colorLinks) {
+                await browserPage.goto(colorLink);
+                await browserPage.waitForSelector('#productTitle');
+                const newDetailedPageHtml = await browserPage.content();
+                const newDetailedHtml = cheerio.load(newDetailedPageHtml);
+                const productDetails = extractProductDetails(newDetailedHtml, colorLink);
+                colorProductDetails.push(productDetails);
+                console.log(`${productDetails.productName} - Done`);
+            }
+
+
+            detailedProductData.push({productColors: colorProductDetails});
         }
 
+        console.log('Scraping complete.');
 
-        detailedProductData.push({ productColors: colorProductDetails });
+        fs.writeFileSync(`data/${category}.json`, JSON.stringify(detailedProductData, null, 4));
     }
 
-    console.log('Scraping complete.');
-
-    fs.writeFileSync(`data/${CATEGORY}.json`, JSON.stringify(detailedProductData, null, 4));
 
     await browserInstance.close();
 })();
